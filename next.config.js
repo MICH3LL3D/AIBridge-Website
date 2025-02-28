58957df2485a06695a60c7b471e281552ac887a5
MichelleDong <michelledong@MichelleDongs-MacBook-Air.local>
2025-02-28 09:03:30 -0800

    Configure Github Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',  // Enable static exports
+  basePath: '/aibridge-website', // Replace with your repo name
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
 }
 
